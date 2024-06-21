f8633ee8bc82e6d7cfb1e8461e674b1b6003477a
User <1>
2024-06-21 16:49:10 +0300

    messenger 4096

--- messengers.py
+++ messengers.py
@@ -8,12 +8,14 @@ admin_tg = os.getenv('admin_tg')
 tg_token_salon = os.getenv('tg_token_salon')
 tg_token_tools = os.getenv('tg_token_tools')
 DO_SEND_TO_BOT = True if os.getenv('DO_SEND_TO_BOT') == 'True' else False
+TG_MAX_MESSAGE_LENGTH = 4096
 
 bot = telebot.TeleBot(tg_token_salon)
 bot_tools = telebot.TeleBot(tg_token_tools)
 
 
 def send_tg_message(text: str, *users: int):
+    text = text[0:TG_MAX_MESSAGE_LENGTH]
     if DO_SEND_TO_BOT:
         for user in users:
             try:
@@ -26,5 +28,6 @@ def send_tg_message(text: str, *users: int):
 
 
 def send_service_tg_message(text: str):
+    text = text[0:TG_MAX_MESSAGE_LENGTH]
     if DO_SEND_TO_BOT:
         bot_tools.send_message(admin_tg, text)
