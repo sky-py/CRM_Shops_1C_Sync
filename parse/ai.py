35810d42c42e873743932e8a8fe2c046be27ce73
sky-py <it.simpl@gmail.com>
2024-11-30 12:47:17 +0200

    chnged AI prompt

--- parse/ai.py
+++ parse/ai.py
@@ -1,7 +1,5 @@
-from asyncio import timeout
-
-import constants
 from openai import OpenAI
+import constants
 
 client = OpenAI(api_key=constants.OPENAI_UKRSALON_API_KEY)
 model = 'gpt-4o-mini'
@@ -11,16 +9,18 @@ def ai_reorder_names(fio: str) -> str:
     completion = client.chat.completions.create(
         model= model,
         messages=[
-            {'role': 'system', 'content': 'Ты дотошный нотариус'},  # <-- This is the system message that provides context to the model
+            {'role': 'system', 'content': 'Ты дотошный нотариус'},
             {'role': 'user', 'content': 'Тебе необходимо записать в правильной последовательности данные клиента. '
                                         'Последовательность такая:\nФамилия Имя Отчество\n'
                                         'В ответе надо указать ИСКЛЮЧИТЕЛЬНО Фамилию Имя Отчество клиента.\n'
                                         'Если какой-то из элеметов ФИО отсутсвует, то его не надо указывать.\n'
                                         'Если в переданных данных присутсвует какое-то посторонее слово, '
-                                        'кроме фамилии, имени, отчества, тогда твой ответ - это пустая строка '
-                                        '(не нужно передавать часть слов или символов!) .\n'
+                                        'кроме фамилии, имени, отчества, тогда твой ответ - это пустая строка. '
+                                        'НЕ НУЖНО ПЕРЕДАВАТЬ ЧАСТЬ СЛОВ ИЛИ СИМВОЛОВ! Ты либо передаёшь ВСЕ слова '
+                                        'в правильной последовательности, либо пустую строку!\n'
+                                        'НЕ НУЖНО ВКЛЮЧАТЬ В ОТВЕТ ОБЪЯСНЕНИЯ!\n'
                                         'Итак, вот данные клиента:\n'
-                                        '{fio}'.format(fio=fio)}   # <-- This is the user message for which the model will generate a response
+                                        '{fio}'.format(fio=fio)}
         ],
         timeout=timeout
     )
